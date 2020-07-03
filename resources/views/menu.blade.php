87242777adbdd46cdbde7aaa9012aa22f10df5b0
schweppe nicolas <nicolas_vialidad@hotmail.com>
2020-07-03 17:58:54 -0300

    se modifico el focus en menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -16,20 +16,20 @@
 
 <nav class="p-1 mb-2 bg-primary text-white" >
   <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" style="color:white" href="{{route('inicio')}}">Inicio</a>
-      <a class="navbar-brand" style="color:white"  href="{{route('cargarArchivos')}}">Cargar Archivo</a>
-      <a class="navbar-brand" style="color:white"  href="{{ route('verArchivo') }}">Ver Archivos</a>
-      <a class="navbar-brand" style="color:white" href="{{ route('usuarios') }}">Usuarios</a>
-    </div>
+    <div class="focus">
+      <a class="navbar-brand" href="{{route('inicio')}}">Inicio</a>
+      <a class="navbar-brand" href="{{route('cargarArchivos')}}">Cargar Archivo</a>
+      <a class="navbar-brand" href="{{ route('verArchivo') }}">Ver Archivos</a>
+      <a class="navbar-brand" href="{{ route('usuarios') }}">Usuarios</a>
+    
+    
   <ul class="nav navbar-nav navbar-right">
-    <a class="navbar-brand" style="color:white" href="#">
+    <a class="navbar-brand" href="#">
     <svg width="1em" height="1em" viewBox="0 0 16 12" class="bi bi-person-check-fill"  fill="currentColor" >
       <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9.854-2.854a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
     </svg> Usuario
   </a>  
-    
-    <a class="navbar-brand" style="color:white" href="#"><svg width="1em" height="1em" viewBox="2 0 16 15" class="bi bi-door-open" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <a class="navbar-brand" href="#"><svg width="1em" height="1em" viewBox="2 0 16 15" class="bi bi-door-open" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
       <path fill-rule="evenodd" d="M1 15.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5zM11.5 2H11V1h.5A1.5 1.5 0 0 1 13 2.5V15h-1V2.5a.5.5 0 0 0-.5-.5z"/>
       <path fill-rule="evenodd" d="M10.828.122A.5.5 0 0 1 11 .5V15h-1V1.077l-6 .857V15H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117z"/>
       <path d="M8 9c0 .552.224 1 .5 1s.5-.448.5-1-.224-1-.5-1-.5.448-.5 1z"/>
@@ -42,6 +42,7 @@
       <button type="submit" class="btn btn-default">Buscar</button>
     </form>
   </div>
+</div>
 </nav>
 
 <div class="container">
