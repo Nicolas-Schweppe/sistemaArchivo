552992f17162a6a7e1368f5f626374ea336f8a7a
schweppe nicolas <nicolas_vialidad@hotmail.com>
2020-07-03 19:02:13 -0300

    se agrego vista cargaArchivo y footer

--- resources/views/cargarArchivos.blade.php
+++ resources/views/cargarArchivos.blade.php
@@ -1,5 +1,46 @@
 @extends('menu')
 
 @section('pagina')
-    <h1>cargas</h1>
+<div class="subtitulos"><h2>Cargas de Archivo <img  src="/iconos/icons/File-earmark-text.svg" alt="" width="20" height="25" title="Bootstrap"></h2></div>
+</br>
+</br>
+</br>
+<div class="formulario">
+    <form>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="nombreArchivo">Nombre del Archivo</label>
+            <input type="nombre" class="form-control" id="nombreArchivo" placeholder="Nombre">
+          </div>
+          <div class="form-group col-md-2">
+            <label for="fechaDocumento">Año del Documento</label>
+            <input type="date" class="form-control" id="fechaDocumento" >
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-2">
+                <?php $fechaActual = date('d-m-Y');?>
+              <label for="fechaCarga">Fecha de Carga</label>
+              <input readonly="readonly" class="form-control" id="fechaCarga" value=<?php echo $fechaActual?>>
+            </div>
+          </div>
+        </div>
+        <div class="form-group col-md-6">
+          <label for="descripcion">Descripcion</label>
+          <input type="text" class="form-control" id="Descripcion" placeholder="Descripcion">
+        </div>
+        <div class="form-group col-md-4">
+          <label for="localidad">Localidad de origen</label>
+          <input type="text" class="form-control" id="localidad" placeholder="Lugar de origen">
+        </div>
+        <div class="form-group col-md-2">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="gridCheck">
+            <label class="form-check-label" for="gridCheck">
+              Check me out
+            </label>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </div>
+        </div>
+      </form>
+    </div>
 @endsection
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
   <link rel="stylesheet" href="css/bootstrap.min.css"/>
   <link rel="stylesheet" type="text/css" href="/css/subtitulos.css" media="all">
+  <link rel="stylesheet" type="text/css" href="/css/footer.css" media="all">
   <link rel="stylesheet" type="text/css" href="css/datatable.min.css"/>
 
 </head>
@@ -44,15 +45,20 @@
   </div>
 </div>
 </nav>
-
 <div class="container">
 
   @yield('pagina')
 
 </div>
-
+<div class="pie">
+  <footer>
+    <img src="imagenes/vialidad.png"  >
+  </footer> 
+</div>
 </body>
+
 <script type="text/javascript" src="js/datatable.min.js"></script>
+
 </html>
 
    
